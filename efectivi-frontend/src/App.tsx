a37e93a42b4c69e3a44a87ae35a7b1653ee038fc
cdperez <cdperez.85@gmail.com>
2023-10-28 01:06:03 +0200

    structure

---
+++ efectivi-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+
+const App = () => {
+
+  return (
+    <>
+      <Header />
+      <h1>Hola</h1>
+    </>
+  )
+}
+
+export default App
---
+++ efectivi-frontend/src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Efectiviwonders</h1>
+      <nav>
+        <ul>
+          <li>Quienes somos</li>
+          <li>Repertorio</li>
+          <li>Calendario</li>
+          <li>Contacto</li>
+          <li><button>Quiero mi presupuesto</button></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
